810eeb7475245142a3f5d31e9a4170c6abebaec7
Singh M Yashwanth (MS/ECL2) <iyg1kor@KOR-V-002EL.kor.apac.bosch.com>
2024-01-10 19:54:17 +0530

    target was removed

--- target/debug/build/vajra_flash_driver-740973f602b6a7ab/build_script_build-740973f602b6a7ab.d
+++
@@ -1,6 +0,0 @@
-/home/iyg1kor/projects/Shakti/riscv_spi_flash_driver/target/debug/build/vajra_flash_driver-740973f602b6a7ab/build_script_build-740973f602b6a7ab: build.rs memory.x
-
-/home/iyg1kor/projects/Shakti/riscv_spi_flash_driver/target/debug/build/vajra_flash_driver-740973f602b6a7ab/build_script_build-740973f602b6a7ab.d: build.rs memory.x
-
-build.rs:
-memory.x:
